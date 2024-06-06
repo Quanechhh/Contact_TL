9bbdc93099b19aee6c83a75801e2860f95ac8759
Quanechhh <quan16143@gmail.com>
2024-06-06 08:44:06 +0700

    Update1

--- app/src/main/java/com/example/contact_tl/MainActivity.java
+++ app/src/main/java/com/example/contact_tl/MainActivity.java
@@ -1,75 +1,44 @@
 package com.example.contact_tl;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import androidx.activity.EdgeToEdge;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        Button btnUnitList = findViewById(R.id.btnUnitList);
-        Button btnEmployeeList = findViewById(R.id.btnEmployeeList);
-        Button btnSearch = findViewById(R.id.btnSearch);
-        Button btnAddUnit = findViewById(R.id.btnAddUnit);
-        Button btnAddEmployee = findViewById(R.id.btnAddEmployee);
-
-        btnUnitList.setOnClickListener(new View.OnClickListener() {
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, UnitListActivity.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.navigation_departments:
+                        selectedFragment = new DepartmentListFragment();
+                        break;
+                    case R.id.navigation_employees:
+                        selectedFragment = new EmployeeListFragment();
+                        break;
+                }
+                if (selectedFragment != null) {
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frame_container, selectedFragment);
+                    transaction.commit();
+                }
+                return true;
             }
         });
 
-        btnEmployeeList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, EmployeeListActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btnSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btnAddUnit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddUnitActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btnAddEmployee.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddEmployeeActivity.class);
-                startActivity(intent);
-            }
-        });
+        // Load the default fragment
+        if (savedInstanceState == null) {
+            navigation.setSelectedItemId(R.id.navigation_departments);
+        }
     }
 }
